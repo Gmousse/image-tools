049b07db7b91a9a8375d8438597105a9f8e6a87c
Guillaume Mousnier <guillaume.mousnier@datawizard.fr>
2026-01-20 17:04:44 +0100

    fix: fixing scandir usage

--- src/image_tools/files.py
+++ src/image_tools/files.py
@@ -6,7 +6,7 @@ def list_all_files_recursively(dir_path: "Path"):
     files = scandir(dir_path)
     for file in files:
         file_path = Path(file.path)
-        if file.is_dir:
+        if file.is_dir():
             yield from list_all_files_recursively(file_path)
-        if file.is_file:
+        if file.is_file():
             yield file_path
